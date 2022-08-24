18f0a7722e18d58341a3afc6b084df34d8f356a7
DIRRHUB <bibishka117@gmail.com>
2022-08-24 23:36:09 +0300

    HistoryCard date

--- lib/common/const/const.dart
+++ lib/common/const/const.dart
@@ -3,4 +3,6 @@ class Const {
   static const String whiteImage = "https://i.ibb.co/2qQqzdR/e.png";
   static const double deliveryPrice = 50;
   static const String empty = "empty";
+  static const String longDateFormat = "mm:hh:ss dd-MM-yyyy";
+  static const String shortDateFormat = "dd-MM-yyyy";
 }
